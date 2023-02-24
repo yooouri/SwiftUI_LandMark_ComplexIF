06c38d173fb0afbafec11fdbed639a85302b24bf
yoouri <pswja06@naver.com>
2023-02-24 16:59:32 +0900

    init

---
+++ ComplexInterface/ComplexInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexInterfaceApp.swift
+//  ComplexInterface
+//
+//  Created by yuri on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ComplexInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CategoryHome()
+        }
+    }
+}
